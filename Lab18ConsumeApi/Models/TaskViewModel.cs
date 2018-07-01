b2d79e9ff7d68b47d07844dbff660908d6623331
Eric Singleton <eric.singleton.jr@gmail.com>
2018-07-01 11:34:35 -0700

    Added todoitem CRUD operations interaction with deployed API

---
+++ Lab18ConsumeApi/Models/TaskViewModel.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab18ConsumeApi.Models
+{
+    public class TaskViewModel
+    {
+        public IEnumerable<SelectListItem> ToDoListNames { get; set; }
+        public IEnumerable<ToDoList> ToDoLists { get; set; }
+        public ToDoItem ToDo { get; set; }
+
+        public static TaskViewModel GetDetails(long id, string list, string task)
+        {
+            TaskViewModel tvm = new TaskViewModel();
+
+            string jToken = JToken.Parse(list).ToString();
+            var lists = JsonConvert.DeserializeObject<List<ToDoList>>(jToken);
+
+            string jToken2 = JToken.Parse(task).ToString();
+            tvm.ToDo = JsonConvert.DeserializeObject<ToDoItem>(jToken2);
+
+            tvm.ToDoListNames = new SelectList(
+                lists.Select(n => n.Name)
+                );
+            tvm.ToDoLists = lists;
+            //tvm.ToDo = item.Where(i => i.ID == id).Single();
+            return tvm;
+        }
+    }
+}
---
+++ Lab18ConsumeApi/Models/ToDoList.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab18ConsumeApi.Models
+{
+    public class ToDoList
+    {
+        public int ID { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public IEnumerable<ToDoItem> ToDoItems { get; set; }
+        [Required]
+        public bool IsDone { get; set; }
+    }
+}
